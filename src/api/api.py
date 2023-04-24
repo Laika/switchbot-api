06c6836dee462ef5e2dd5528e0fbc4ba23e1b91f
Laika <laika@albina.cc>
2023-04-24 17:08:26 +0900

    Add human detecting sensor

--- src/api/api.py
+++ src/api/api.py
@@ -8,7 +8,23 @@ app = FastAPI()
 @app.get("/api/thermometer")
 async def all():
     try:
-        with open("/data/sensor.json") as f:
+        with open("/data/thermometer.json") as f:
+            sensor = json.load(f)
+    except Exception:
+        sensor = {
+            "mac": None,
+            "sensor_type": "SwitchBot",
+            "temperature": None,
+            "humidity": None,
+            "battery": None,
+        }
+    return sensor
+
+
+@app.get("/api/human-detector")
+async def all():
+    try:
+        with open("/data/human-detector.json") as f:
             sensor = json.load(f)
     except Exception:
         sensor = {
--- src/sensor/sensor.py
+++ src/sensor/sensor.py
@@ -5,13 +5,15 @@ import time
 import schedule
 from bluepy.btle import DefaultDelegate, Scanner
 
-macaddr = os.getenv("MAC_ADDR", "FF:FF:FF:FF:FF:FF").lower()
+thermometer_macaddr = os.getenv("MAC_ADDR", "FF:FF:FF:FF:FF:FF").lower()
+human_detector_macaddr = os.getenv("HUMAN_DETECTOR_MAC_ADDR", "FF:FF:FF:FF:FF:FF").lower()
+
 period = int(os.getenv("PERIOD", "10"))
 service_uuid = "cba20d00-224d-11e6-9fb8-0002a5d5c51b"
 char_uuid = "cba20002-224d-11e6-9fb8-0002a5d5c51b"
 
 
-class ScanDelegate(DefaultDelegate):
+class ThermometerScanDelegate(DefaultDelegate):
     def __init__(self, macaddr):
         DefaultDelegate.__init__(self)
         self.sensorValue = None
@@ -33,7 +35,7 @@ class ScanDelegate(DefaultDelegate):
         if not isTemperatureAboveFreezing:
             temp = -temp
         humid = valueBinary[5] & 0b01111111
-        with open("/data/sensor.json", "w") as f:
+        with open("/data/thermometer.json", "w") as f:
             sensor_value = {
                 "mac": self.macaddr,
                 "sensor_type": "SwitchBot",
@@ -44,9 +46,36 @@ class ScanDelegate(DefaultDelegate):
             json.dump(sensor_value, f)
 
 
+class HumanDetectorScanDelegate(DefaultDelegate):
+    def __init__(self, macaddr):
+        DefaultDelegate.__init__(self)
+        self.sensorValue = None
+        self.macaddr = macaddr
+
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if dev.addr != self.macaddr:
+            return
+        for adtype, desc, value in dev.getScanData():
+            if desc == "16b Service Data":
+                if adtype == 22:
+                    self._decodeSensorData(value)
+
+    def _decodeSensorData(self, valueStr):
+        global sensor_value
+        valueBinary = bytes.fromhex(valueStr[4:])
+        isON = (valueBinary[1] & 0b01000000) >> 6
+        time = valueBinary[4]
+        isIlluminance = (valueBinary[5] & 0b00000011) - 1
+
+        with open("/data/human-detector.json", "w") as f:
+            sensor_value = {"mac": self.macaddr, "sensor_type": "SwitchBot", "isIllminance": isIlluminance, "isOn": isON, "time": time}
+            print(sensor_value)
+            json.dump(sensor_value, f)
+
+
 def scan():
-    scanner = Scanner().withDelegate(ScanDelegate(macaddr))
-    scanner.scan(5.0)
+    Scanner().withDelegate(HumanDetectorScanDelegate(human_detector_macaddr)).scan(5.0)
+    Scanner().withDelegate(ThermometerScanDelegate(thermometer_macaddr)).scan(5.0)
 
 
 schedule.every(period).seconds.do(scan)
